3d7f1783751ed433d201022d2a1dc76bfafce5e9
Семён <floan732@gmail.com>
2022-07-05 19:57:28 +0300

    Homework

--- my-spring-boot-jdbc-foundations/src/main/resources/data.sql
+++ my-spring-boot-jdbc-foundations/src/main/resources/data.sql
@@ -2,3 +2,8 @@ INSERT INTO faculties(id, fName) VALUES (1, 'Java Development');
 INSERT INTO faculties(id, fName) VALUES (2, 'Java Automation');
 
 INSERT INTO users(id, name, age, faculty_id) VALUES ( 1, 'Ivan', 21, 1);
+
+INSERT INTO breeds(id, name) VALUES (1, 'Bulldog');
+INSERT INTO pets(id, breed_id) VALUES (1, 1);
+
+
